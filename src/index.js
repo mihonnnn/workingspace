a00678f3eee514062b20c6f9301ace2b49c3c4fd
mihonnn <98961771+mihonnnn@users.noreply.github.com>
2022-05-11 22:42:56 +0900

    0511commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const onClick = () => {
+  var num = document.getElementById("data").value;
+  const result = (num * 600 + 9480) / num;
+  const div = document.createElement("div");
+  const resultText = `現在、一日あたり${result}円です。`;
+  div.innerText = resultText;
+  console.log(div);
+  //document.getElementById("resultbox").appendChild(div);
+};
+
+document.getElementById("calc").addEventListener("click", () => onClick());
